c0f52110647ea065305bb409536a589121ef7536
Vaishnavi11656 <vaishnavigajjala80@gmail.com>
2026-04-28 16:51:08 +0530

    fix: socket.js and client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:4000/api/v1",
+    baseURL: import.meta.env.VITE_API_URL
+        ? `${import.meta.env.VITE_API_URL} / api / v1`
+        : "http://localhost:4000/api/v1",
     withCredentials: true,
 });
 api.interceptors.response.use((response) => response, async (error) => {
